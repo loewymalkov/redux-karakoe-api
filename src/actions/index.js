619c2a941bbe701ca09e2c5d6516d25f8960c5f7
Student <example@example.com>
2019-12-09 11:14:21 -0800

    add receive lyrics case

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
   
-import * as types from "./../constants/ActionTypes";
+import * as types from './../constants/ActionTypes';
 import v4 from 'uuid/v4';
 
 export const nextLyric = (currentSongId) => ({
@@ -20,12 +20,38 @@ export const changeSong = (newSelectedSongId) => ({
 export function fetchSongId(title) {
   return function (dispatch) {
     const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
     title = title.replace(' ', '_');
     return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=88e81093f32a1a1b75e0b5372872f16f').then(
       response => response.json(),
       error => console.log('an error occurred.', error)
     ).then(function(json) {
-      console.log('check out this sick api response', json)
+      console.log('check out this sick api response', json);
     });
   };
 }
+
+export const requestSong = (title, localSongId) => ({
+  type: types.REQUEST_SONG,
+  title,
+  songId: localSongId
+});
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) {
+  return fetch('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + musicMatchId + '&apikey=88e81093f32a1a1b75e0b5372872f16f').then(
+    response => response.json(),
+    error => console.error('An error occurred', error)
+  ).then(function(json) {
+    console.log('hey wow api 2', json);
+  });
+}
+
+export const receiveSong = (title, artist, songId, songArray) => ({
+  type: types.RECEIVE_SONG,
+  songId,
+  title,
+  artist,
+  songArray,
+  receivedAt: Date.now()
+});
+
